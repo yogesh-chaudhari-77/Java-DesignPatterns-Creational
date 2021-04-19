4c975baac38359a5a113e9d393bb73a5d67df688
yogesh.chaudhari.77 <chaudhari.yogesh20@gmail.com>
2021-04-19 22:27:01 +1000

    Push singleton pattern demo

---
+++ src/singleton/Main.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SingletonClass instance1 = SingletonClass.getInstance();
+		SingletonClass instance2 = SingletonClass.getInstance();
+		SingletonClass instance3 = SingletonClass.getInstance();
+		SingletonClass instance4 = SingletonClass.getInstance();
+		SingletonClass instance5 = SingletonClass.getInstance();
+		
+	}
+
+}
